bc1effc20d3ad91c26e165a3d87604ba2112e8de
RoiVigo91 <roioliveira71@gmail.com>
2025-02-06 09:30:12 +0100

    Ejercicio28Resuelto

--- src/main/java/com/campusdual/classroom/Exercise28.java
+++ src/main/java/com/campusdual/classroom/Exercise28.java
@@ -1,7 +1,7 @@
 package com.campusdual.classroom;
 
 public class Exercise28 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws  RuntimeException{
         throw new RuntimeException("Excepción lanzada (ERROR! 86558744.)");
     }
 }
